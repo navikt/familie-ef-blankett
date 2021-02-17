180e805f551e6a690e1ae9cd1d4ff7f3726e173e
charliemidtlyng <charlie.midtlyng@bekk.no>
2021-02-17 11:40:35 +0100

    Lagt inn dummypdf-rute

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -5,11 +5,17 @@ import { genererPdf } from './utils/api';
 import { Feil } from './utils/Feil';
 import { logError, logSecure } from '@navikt/familie-logging';
 
+import fs from 'fs';
+
 const router = express.Router();
 
 router.get('/status', (_, res) => {
   res.status(200).end();
 });
+
+const lesMockFil = () => {
+  return fs.readFileSync('./src/server/mock/dummydata.json', 'UTF-8');
+};
 
 router.post('/html', async (req: Request, res: Response) => {
   const dokument: IDokumentData = req.body as IDokumentData;
@@ -25,6 +31,18 @@ router.post('/html', async (req: Request, res: Response) => {
     logError(`Generering av dokument (html) feilet: ${feil.message}`);
     loggFeilMedDataTilSecurelog<IDokumentData>(dokument, req, feil);
     return res.status(500).send(`Generering av dokument (html) feilet: ${feil.message}`);
+  }
+});
+
+router.post('/dummy-pdf', async (_req: Request, res: Response) => {
+  try {
+    const html = await hentDokumentHtml(lesMockFil());
+    const pdf = await genererPdf(html);
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', `attachment; filename=saksbehandlingsblankett.pdf`);
+    res.end(pdf);
+  } catch (feil) {
+    return res.status(500).send(`Generering av dokument (pdf) feilet: ${feil.message}`);
   }
 });
 
