82a0b38417c4cb3b8a5f1c14ac5b6d77f0f0b53e
Viktor <viktor.grondalen.solberg@nav.no>
2023-04-20 13:10:58 +0200

    WIP

--- webpack.config.server.js
+++ webpack.config.server.js
@@ -1,36 +1,40 @@
-const path = require("path");
-const nodeExternals = require("webpack-node-externals");
+import path from 'path';
+import nodeExternals from 'webpack-node-externals';
 
-const entry = { server: "./src/server/index.ts" };
+const entry = { server: './src/server/index.ts' };
 
-module.exports = {
-  mode: process.env.NODE_ENV ? process.env.NODE_ENV : "development",
-  target: "node",
-  devtool: "inline-source-map",
+const webpackConfig = {
+  mode: process.env.NODE_ENV ? process.env.NODE_ENV : 'development',
+  target: 'node',
+  devtool: 'inline-source-map',
   entry: entry,
   output: {
-    path: path.resolve(__dirname, "build/backend"),
-    filename: "[name].js",
+    path: path.resolve(process.cwd(), 'build/backend'),
+    filename: '[name].js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js"],
+    extensions: ['.ts', '.tsx', '.js'],
   },
   // don't compile node_modules
   externals: [nodeExternals()],
   module: {
     rules: [
       {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: "ts-loader",
-            options: {
-              // use the tsconfig in the server directory
-              configFile: "src/server/tsconfig.json",
-            },
-          },
-        ],
+        test: /\.(jsx|tsx|ts|js)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: ['react-app'],
+        },
+      },
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
+        },
       },
     ],
   },
 };
+
+export default webpackConfig;
