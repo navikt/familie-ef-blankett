f4bf1f6dcc55b9487b252486998b678f4b7662fa
Viktor <viktor.grondalen.solberg@nav.no>
2023-05-04 11:17:55 +0200

    skal vise dato for når g-beløp gjelder fra for å unngå misforståelser

--- src/typer/dokumentApi.ts
+++ src/typer/dokumentApi.ts
@@ -647,7 +647,7 @@ export const delvilkårTypeTilTekst: Record<IRegelId, string> = {
   HAR_TIDLIGERE_MOTTATT_OVERGANSSTØNAD: 'Har søker tidligere mottatt overgangsstønad?',
   ER_I_ARBEID_ELLER_FORBIGÅENDE_SYKDOM: 'Er brukeren i arbeid eller har forbigående sykdom?',
   INNTEKT_LAVERE_ENN_INNTEKTSGRENSE:
-    'Har brukeren inntekt under 6 ganger grunnbeløpet (638 394 kr / 53 200 kr)?',
+    'Har brukeren inntekt under 6 ganger grunnbeløpet (pr 01.05.22 668 862 kr / 55 738 kr)?',
   INNTEKT_SAMSVARER_MED_OS:
     'Er inntekten i samsvar med den inntekten som er lagt til grunn ved beregning av overgangsstønad?',
   HAR_ALDER_LAVERE_ENN_GRENSEVERDI: 'Har barnet fullført 4.skoleår?',
