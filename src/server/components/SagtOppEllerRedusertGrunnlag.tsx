2dd4c48219d589b65311830a0401f2cb93f645bc
Ole Christian Kvernberg <ole.christian.kvernberg@nav.no>
2023-09-29 10:10:24 +0200

    Vis harAvsluttetArbeidsforhold under sagt opp eller redusert arbeidsforhold

--- src/server/components/SagtOppEllerRedusertGrunnlag.tsx
+++ src/server/components/SagtOppEllerRedusertGrunnlag.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
-const SagtOppEllerRedusertGrunnlag = () => {
+interface Props {
+  harAvsluttetArbeidsforhold: boolean;
+}
+
+const SagtOppEllerRedusertGrunnlag: React.FC<Props> = ({ harAvsluttetArbeidsforhold }) => {
   return (
     <>
-      <h3>Grunnlag</h3>
-      <div>Ingen registergrunnlag</div>
+      <h3>Registerdata</h3>
+      <div>
+        Har avsluttet arbeidsforhold siste 6 mnd: {booleanTilTekst(harAvsluttetArbeidsforhold)}
+      </div>
     </>
   );
 };
 
 export default SagtOppEllerRedusertGrunnlag;
+
+const booleanTilTekst = (b: boolean): string => {
+  if (b) return 'Ja';
+  else return 'Nei';
+};
