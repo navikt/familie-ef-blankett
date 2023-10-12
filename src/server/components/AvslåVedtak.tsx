63c696ad80b2c4f54f94721a2a2266c15f410102
gunnsteingarmo <141132903+gunnsteingarmo@users.noreply.github.com>
2023-10-12 09:05:23 +0200

    Vise arsak avslag (#197)
    Ønsker å vise årsak avslag i blankett.

--- src/server/components/AvslåVedtak.tsx
+++ src/server/components/AvslåVedtak.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { IAvslåVedtak } from '../../typer/dokumentApi';
+import { IAvslåVedtak, avslagÅrsakTilTekst } from '../../typer/dokumentApi';
 
-export const AvslåVedtak: React.FC<IAvslåVedtak> = ({ avslåBegrunnelse }) => {
+export const AvslåVedtak: React.FC<IAvslåVedtak> = ({ avslåÅrsak, avslåBegrunnelse }) => {
   return (
     <div className={'page-break'}>
       <h2>Vedtak</h2>
       <h3>Resultat</h3>
       <div>Avslå</div>
+      <h4>Årsak</h4>
+      <p>{avslagÅrsakTilTekst[avslåÅrsak]}</p>
       <h4>Begrunnelse</h4>
       <p style={{ whiteSpace: 'pre-wrap' }}>{avslåBegrunnelse}</p>
     </div>
